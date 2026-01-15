c79d88eab7ad7afe2102d046a73decbce9137952
Abdellah Hammouch <hammouchabdellah4529@gmail.com>
2026-01-15 15:05:23 +0100

    Add Composer autoload

---
+++ config/config.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'db' => [
+        'driver' => 'pgsql',
+        'host'   => '127.0.0.1',
+        'port'   => 5432,
+        'name'   => 'coach_pro',
+        'user'   => 'coachpro',
+        'pass'   => 'abha11228899',
+    ],
+];
+?>
---
+++ core/Database.php
@@ -0,0 +1,30 @@
+<?php
+
+
+class Database
+{
+    private static ?PDO $pdo = null;
+
+    public static function getConnection(): PDO
+    {
+        if (self::$pdo === null) {
+            $config = require __DIR__ . '/../config/config.php';
+            $db = $config['db'];
+
+            $dsn = sprintf(
+                "pgsql:host=%s;port=%d;dbname=%s",
+                $db['host'],
+                $db['port'],
+                $db['name']
+            );
+            
+            self::$pdo = new PDO($dsn, $db['user'], $db['pass'], [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            ]);
+        }
+
+        return self::$pdo;
+    }
+}
+?>
